76eeb8c0efb51590a1ed74beb85e9def4e5df429
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2024-01-10 17:59:16 +0530

    Add-Electrical-Items-form

---
+++ src/Controllers/AddElectricalItemsForm.java
@@ -0,0 +1,75 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AddElectricalItemsForm {
+
+    @FXML
+    private AnchorPane pane3;
+
+    @FXML
+    private TextField pnametxt;
+
+    @FXML
+    private TextField pidtxt;
+
+    @FXML
+    private TextField ppricetxt;
+
+    @FXML
+    private TextField pqtytxt;
+
+    @FXML
+    private TableView<?> electricalitemTable;
+
+    @FXML
+    private TableColumn<?, ?> pid;
+
+    @FXML
+    private TableColumn<?, ?> pname;
+
+    @FXML
+    private TableColumn<?, ?> pprice;
+
+    @FXML
+    private TableColumn<?, ?> pqty;
+
+    @FXML
+    private Button savebtn;
+
+    @FXML
+    private Button clearbtn;
+
+    @FXML
+    private Button refreshbtn;
+
+    @FXML
+    private Button Backbtn;
+
+    @FXML
+    void BackbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClearbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RefreshbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SavebuttonOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/Controllers/AddItemschooseForm.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AddItemschooseForm {
+
+    @FXML
+    private Button electricalbtn;
+
+    @FXML
+    private Button electronicbtn;
+
+    @FXML
+    void ElectricalbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ElectronicbuttonOnAction(ActionEvent event) {
+
+    }
+
+}
