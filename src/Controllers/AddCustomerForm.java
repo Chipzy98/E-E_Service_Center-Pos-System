bc9420d82d87d9ffe9226b99ce27cf57b4445719
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2024-01-10 19:42:03 +0530

    Add-customer-form

---
+++ src/Controllers/AddCustomerForm.java
@@ -0,0 +1,50 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+public class AddCustomerForm {
+
+    @FXML
+    private TextField cusnametxt;
+
+    @FXML
+    private TextField cusidtxt;
+
+    @FXML
+    private TextField cusemailtxt;
+
+    @FXML
+    private TextField cusmnumtxt;
+
+    @FXML
+    private Button savebtn;
+
+    @FXML
+    private Button clearbtn;
+
+    @FXML
+    private TextField cuspswtxt;
+
+    @FXML
+    private Button backbtn;
+
+    @FXML
+    void BackbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClearbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SavebuttonOnAction(ActionEvent event) {
+
+    }
+
+}
