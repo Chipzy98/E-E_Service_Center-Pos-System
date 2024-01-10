75675bb45db1f6ce1549984a1cb973e722fbbc0a
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2024-01-10 16:44:18 +0530

    Login page

---
+++ src/Controllers/MainInterfaceController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainInterfaceController {
+
+    @FXML
+    private Button loginbtn;
+
+    @FXML
+    private Button clearbtn;
+
+    @FXML
+    void ClearbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LoginbuttonOnAction(ActionEvent event) {
+
+    }
+
+}
