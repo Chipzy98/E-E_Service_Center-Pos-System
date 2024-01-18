9060c6e55707a73dc593a79e489090c9918f9eae
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2024-01-18 17:47:15 +0530

    Dashboard-Form

---
+++ src/Controllers/CustomerDashboardForm.java
@@ -0,0 +1,33 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CustomerDashboardForm {
+
+    @FXML
+    private Button logoutbtn;
+
+    @FXML
+    private Button porderbtn;
+
+    @FXML
+    private Button oreportbtn;
+
+    @FXML
+    void LogoutbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OreportbuttonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PorderbuttonOnAction(ActionEvent event) {
+
+    }
+
+}
